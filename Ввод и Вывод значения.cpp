07751d0c73cdd9db3d7fd17ea42f99d020424103
Чубукностина <@DESKTOP-GUCFUDB>
2022-12-01 21:32:11 +0300

    Добавьте файлы проекта.

---
+++ Ввод и Вывод значения.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;					// После запуска проги выводится
+										// сообщение "Please enter a number";
+int main()								// затем ожидается ввод пользоватем
+{										// числа и нажатия "Enter"
+	int a;								// Введенное знач. сохр. в переменной "а"
+	cout << "Please enter a number\n";
+	cin >> a;
+
+	return 0;
+}
